ad3a45d983b2d73f5f17362807a1e63160d364a7
Max Fisher <machfour+git@protonmail.com>
2023-10-18 22:21:27 +1100

    add folder for i3blocks uptime

--- i3blocks/uptime/uptime.c
+++ i3blocks/uptime/uptime.c
@@ -3,7 +3,8 @@
 
 // Returns time since system was powered on, in seconds.
 // If /proc/uptime doesn't exist or can't be read, returns -1.
-long get_uptime() {
+// Integer overflow will occur after around 68 years of uptime.
+int get_uptime() {
     float uptime;
     FILE* proc_uptime_file = fopen("/proc/uptime", "r");
     if (proc_uptime_file == NULL) {
@@ -11,11 +12,19 @@ long get_uptime() {
     }
 
     fscanf(proc_uptime_file, "%f", &uptime);
-    return lrintf(uptime);
+    return (int) lrintf(uptime);
 }
 
-void main() {
-    int total_minutes = (int) (get_uptime() / 60L);
+// Prints out system uptime in one of the following formats:
+// XXm, if the system uptime is less than 60 minutes
+// XXh XXm, if the system uptime is less than 24 hours
+// XXd XXh XXm, otherwise
+int main() {
+    int uptime = get_uptime();
+    if (uptime == -1) {
+        printf("oops\n");
+    }
+    int total_minutes = uptime / 60;
     int total_hours = total_minutes / 60;
     int minutes = total_minutes % 60;
     int hours = total_hours % 24;
@@ -24,8 +33,10 @@ void main() {
     if (days > 0) {
         printf("%dd %2dh %2dm\n", days, hours, minutes);
     } else if (hours > 0) {
-        printf("%2dh %2dm\n", hours, minutes);
+        printf("%dh %2dm\n", hours, minutes);
     } else {
-        printf("%2dm\n", minutes);
+        printf("%dm\n", minutes);
     }
+
+    return 0;
 }
